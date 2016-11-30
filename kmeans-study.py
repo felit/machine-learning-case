e1fa1c4b1e2fd9793d1711e414bf640f18c30e27
felit <nofelit@126.com>
2016-11-30 14:54:34 +0800

    init

---
+++ kmeans-study.py
@@ -0,0 +1,19 @@
+#-*- coding:utf8 -*-
+import numpy as np
+
+
+def get_k_point():
+	pass
+
+
+def devide_points():
+	pass
+
+def new_k_point():
+	pass
+
+def finished_kmeans():
+	pass
+
+if __name__=='__main__':
+	pass
